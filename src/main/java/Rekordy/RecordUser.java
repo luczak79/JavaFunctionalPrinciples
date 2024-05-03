e3cd137b82fbd60581bb442e326ac57a0944a115
marcin.luczak <marcin.luczak@pricefx.com>
2024-05-03 22:09:53 +0200

    Introduced Builder pattern in RecordUser class and updated test usage.
    In this commit, the canonical constructor in RecordUser.java was commented out and replaced by a static inner Builder class. This Builder class provides a more flexible way to create RecordUser instances. Correspondingly, TestClass.java was updated to create a RecordUser object using this new Builder.

--- src/main/java/Rekordy/RecordUser.java
+++ src/main/java/Rekordy/RecordUser.java
@@ -1,17 +1,16 @@
 package Rekordy;
 
 import java.time.LocalDateTime;
-import java.util.Objects;
 
 public record RecordUser(String username, boolean active, LocalDateTime lastLogin) {
 
-    //Canonical constructor converted to compact form
-    public RecordUser {
-        Objects.requireNonNull(username);
-        Objects.requireNonNull(lastLogin);
-
-        username = username.toLowerCase();
-    }
+//    // Canonical constructor converted to compact form
+//    public RecordUser {
+//        Objects.requireNonNull(username);
+//        Objects.requireNonNull(lastLogin);
+//
+//        username = username.toLowerCase();
+//    }
 
     @Override
     public String username() {
@@ -21,4 +20,30 @@ public record RecordUser(String username, boolean active, LocalDateTime lastLogi
         return this.username;
     }
 
+
+    // Static inner class for a Builder
+    public static class Builder {
+        private String username;
+        private boolean active;
+        private LocalDateTime lastLogin;
+
+        public Builder withUsername(String username) {
+            this.username = username;
+            return this;
+        }
+
+        public Builder withActive(boolean active) {
+            this.active = active;
+            return this;
+        }
+
+        public Builder withLastLogin(LocalDateTime lastLogin) {
+            this.lastLogin = lastLogin;
+            return this;
+        }
+
+        public RecordUser build() {
+            return new RecordUser(username, active, lastLogin);
+        }
+    }
 }
--- src/main/java/Rekordy/TestClass.java
+++ src/main/java/Rekordy/TestClass.java
@@ -7,5 +7,8 @@ public class TestClass {
         var recordUser = new RecordUser(null, true, LocalDateTime.now());
         var username = recordUser.username();
         System.out.println(username);
+
+        var recordUser2 = new RecordUser.Builder().withActive(true).withUsername("Marcin").withLastLogin(LocalDateTime.now()).build();
+        System.out.println(recordUser2.username());
     }
 }
